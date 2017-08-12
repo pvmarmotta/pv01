5579c11b3982078362ed18c0f9f287354365bfa5
pvmarmotta <pvernetto@gmail.com>
2017-08-12 23:56:28 +0200

    this and that

--- src/org/pierre/Main.java
+++ src/org/pierre/Main.java
@@ -5,5 +5,7 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("Main.main ciao ciao");
+        System.out.println("adding this");
+        System.out.println("adding that");
     }
 }
